28f48972c3f4a2327955d43b34f0b0e56c514879
vcontrerasp13 <projectvcontreras@gmail.com>
2024-11-14 09:46:24 -0500

    error de ean/route.ts

--- src/app/api/ean/route.ts
+++ src/app/api/ean/route.ts
@@ -1,4 +1,4 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 // import mysql from 'mysql2/promise';
 
 // export async function GET() {
@@ -116,3 +116,14 @@
 //         return NextResponse.json({ error: error.message }, { status: 500 });
 //     }
 // }
+
+
+export async function GET() {
+    try {
+        return NextResponse.json({ message: 'METODO GET' }, { status: 200 });
+
+    } catch (error: any) {
+        console.error(error);
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
