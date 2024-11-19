44f22e075124262aeddff93787e42facbaebbeab
kayse2024 <teamdsa@kayser.pe>
2024-11-19 16:57:56 -0500

    ??

--- src/actions/envio/actualizarEnvio.ts
+++ src/actions/envio/actualizarEnvio.ts
@@ -3,7 +3,7 @@
 import { revalidatePath } from "next/cache";
 
 export const onUpdateEnvio = async (
-    ordenes: string[], // Recibimos una lista de órdenes
+    ordenes: string, // Recibimos una lista de órdenes
     estado: string,
     path: string
 ) => {
