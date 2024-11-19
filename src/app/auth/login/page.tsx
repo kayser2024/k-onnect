2b5be5cd769d11c3bc86722aae04e3d8d023add2
kayse2024 <teamdsa@kayser.pe>
2024-11-19 16:29:19 -0500

    quitando texto al login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -11,15 +11,6 @@ export default function LoginPage() {
       </div>
 
       <div className="flex flex-col items-center justify-center mt-4 w-full">
-
-        {/* <h1 className={` text-4xl mb-5 `}>Ingresar</h1> */}
-        {/* <h2 className="font-bold text-2xl  md:text-3xl lg:text-5xl">
-          <span className="underline decoration-dashed decoration-yellow-500 decoration-3 underline-offset-2">Sistema</span> para gestionar pedidos
-        </h2> */}
-        {/* <p className="text opacity-90 lg:text-xl ">
-          Revisas las ordenes de tu tienda, gestiona tus productos, clientes y mucho mas.
-        </p> */}
-
         <LoginForm />
       </div>
     </div>
